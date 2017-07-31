ca57824db3db01feaf32b5e4f01601532636a099
Joseph Knoebel <knoebelja@gmail.com>
2017-07-31 12:29:22 -0400

    joseph 3

--- bash.js
+++ bash.js
@@ -4,16 +4,22 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  let cmd = data.toString().trim(); // remove the newline
+  let cmd = data.toString().trim().split(' '); // remove the newline
 
-  let runCMD = function(){
-    if (commands[cmd]){
-      return commands[cmd]();
-    } else {
-      return 'You typed: ' + cmd;
-    }
+  // let runCMD = function(){
+  //   if (commands[cmd]){
+  //     return commands[cmd]();
+  //   } else {
+  //     return 'You typed: ' + cmd;
+  //   }
+  // }
+  // process.stdout.write(runCMD() + '\nprompt > ');
+
+
+  if (commands[cmd[0]]){
+    commands[cmd[0]](cmd);
+  } else {
+    process.stdout.write('prompt > ');
   }
-  process.stdout.write(runCMD() + '\nprompt > ');
-
 
 });
--- commands.js
+++ commands.js
@@ -2,28 +2,76 @@ let fs = require('fs');
 
 module.exports = {
   pwd: function(){
-    return process.env.PWD;
+    process.stdout.write(process.env.PWD + '\nprompt > ');
   },
 
   date: function(){
-    return new Date().toString();
+    process.stdout.write(new Date().toString() + '\nprompt > ');
   },
 
   ls: function(){
-    let output = '';
-
     fs.readdir('.', function(err, files){
-      if(err) throw err;
-
-      for(let i = 0; i<files.length; i++){
-        console.log(files[i])
-        output = output+files[i]+'\n';
-      }
-      // files.forEach(function(file){
-      //   output = output + file + '\n';
-      // });
+      if (err) throw err;
+      files.forEach(function(file){
+        process.stdout.write(file + '\n');
+      });
+      process.stdout.write('prompt > ');
     });
-    console.log(output)
-    return output;
+  },
+
+  echo: function(args) {
+    let output = '';
+    for (let i = 1; i < args.length; i++){
+      output += args[i];
+      if (i < args.length - 1){
+        output += ' ';
+      }
+    }
+    process.stdout.write(output + '\nprompt > ');
+  },
+
+  cat: function(args) {
+    let fileName = args[1];
+    fs.readFile('./' + fileName, function(err, data) {
+      if (err) throw err;
+      process.stdout.write(data + '\nprompt >');
+    });
+  },
+
+  head: function(args) {
+    let fileName = args[1];
+    let numLines = args[2] || 5;
+    fs.readFile('./' + fileName, function(err, data) {
+      if (err) throw err;
+      let lineByLine = data.toString().trim().split('\n');
+      for (let i = 0; i < numLines; i++){
+        process.stdout.write(lineByLine[i] + '\n');
+      }
+      process.stdout.write('prompt >');
+    });
+  },
+
+  tail: function(args){
+    let fileName = args[1];
+    let numLines = args[2] || 5;
+    fs.readFile('./' + fileName, function(err, data) {
+      if (err) throw err;
+      let lineByLine = data.toString().trim().split('\n');
+      for (let i = lineByLine.length - numLines; i < lineByLine.length; i++){
+        process.stdout.write(lineByLine[i] + '\n');
+      }
+      process.stdout.write('prompt >');
+    });
   }
+
+  // ls: function(){
+  //     let output = '';
+  //     fs.readdirSync('.', function(err, files){
+  //       if (err) throw err;
+  //       files.forEach(function(file){
+  //         output += (file + '\n');
+  //       });
+  //     });
+  //     return output;
+  // }
 }
